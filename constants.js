75529d44cd8bc7d8c09ab28d2cb4052b40da7954
Vasiliy Razdorskiy <vrazdorsky@yandex.ru>
2023-04-10 22:29:32 +0300

    Создать базовые тексты ошибок #18

---
+++ constants.js
@@ -0,0 +1,15 @@
+const errorsTexts = {
+  baseError : 'Произошла ошибка',
+  userNotFound: 'Пользователь не найден',
+  MovieNotFound: 'Фильм не найден',
+  incorrectData: 'Введены некорректные данные',
+  incorrectId: 'Некорректный id',
+  alreadyRegisteredError: 'Пользователь с такими данными уже существует',
+  movieAccessError: 'Взаимодействие с чужим сохранённым фильмом невозможно',
+  needToAuthoriseError: 'Необходима авторизация',
+  incorrectRouteError: 'Некорректный путь',
+}
+
+module.exports = {
+  errorsTexts,
+}
