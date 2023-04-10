559be31505a4a53102edd72fa8ff3804f42a479a
Vasiliy Razdorskiy <vrazdorsky@yandex.ru>
2023-04-10 22:09:57 +0300

    Создать схему и модель User #3

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: "Поле 'email' должно быть валидным email-адресом."
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  }
+}, { versionKey: false });
+
+const userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
